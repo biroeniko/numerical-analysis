d3623f5d16983879c2fcd16b73a0e60c2b1cec9d
Biro Eniko <encybiro@gmail.com>
2018-08-10 18:14:20 +0300

    Corrected license year

--- GaussElimination/GaussElimSolve.m
+++ GaussElimination/GaussElimSolve.m
@@ -1,7 +1,7 @@
 %{
 MIT License
 
-Copyright (c) 2016 Biro Eniko
+Copyright (c) 2018 Biro Eniko
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
